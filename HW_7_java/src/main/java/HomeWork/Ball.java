b37647934c310cfa500ad75c922dd81371525cfb
sam-4igrin <sam-4igrin@ro.ru>
2022-05-03 17:16:08 +0300

    HW_7_JAVA

---
+++ HW_7_java/src/main/java/HomeWork/Ball.java
@@ -0,0 +1,57 @@
+package HomeWork;
+
+import HomeWork.GameCanvas;
+import HomeWork.Sprite;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Ball extends Sprite {
+    private static Random rnd = new Random();
+    private final Color color;
+    private float vX;
+    private float vY;
+
+    Ball() {
+        halfHeight = 20 + (float) (Math.random() * 50f);
+        halfWidth = halfHeight;
+        color = new Color(rnd.nextInt());
+        vX = 100f + (float) (Math.random() * 200f);
+        vY = 100f + (float) (Math.random() * 200f);
+    }
+
+    Ball(int x, int y) {
+        this();
+        this.x = x;
+        this.y = y;
+    }
+
+    void update(GameCanvas canvas, float deltaTime) {
+        x += vX * deltaTime;
+        y += vY * deltaTime;
+
+        if (getLeft() < canvas.getLeft()) {
+            setLeft(canvas.getLeft());
+            vX = -vX;
+        }
+        if (getRight() > canvas.getRight()) {
+            setRight(canvas.getRight());
+            vX = -vX;
+        }
+        if (getTop() < canvas.getTOP()) {
+            setTop(canvas.getTOP());
+            vY = -vY;
+        }
+        if (getBottom() > canvas.getBottom()) {
+            setBottom(canvas.getBottom());
+            vY = -vY;
+        }
+    }
+
+    @Override
+    void render(GameCanvas canvas, Graphics g) {
+        g.setColor(color);
+        g.fillOval((int) getLeft(), (int) getTop(),
+                (int) getWidth(), (int) getHeight());
+    }
+}
---
+++ HW_7_java/src/main/java/HomeWork_4/ServerSocketThread.java
@@ -0,0 +1,26 @@
+package HomeWork_4;
+
+import java.net.ServerSocket;
+
+public class ServerSocketThread extends Thread{
+    private int port;
+
+    ServerSocketThread(String name, int port) {
+        super(name);
+        this.port = port;
+        start();
+    }
+   @Override
+    public void run() {
+       System.out.println("Server started");
+       while (!isInterrupted()) {
+           System.out.println(getName()+ " is running");
+           try {
+               sleep(2000);
+           } catch (InterruptedException e) {
+               interrupt();
+           }
+       }
+       System.out.println("Server stopped");
+   }
+}
